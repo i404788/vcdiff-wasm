2122ecbda655463d0996044600689c8bf48f0ce0
i404788 <404788@student.fontys.nl>
2019-07-25 23:00:22 +0000

    Format and fix bug

--- compat/vcddec.cc
+++ compat/vcddec.cc
@@ -1,39 +1,31 @@
-#include <emscripten.h>
-#include <string>
-#include <google/vcdecoder.h>
 #include "common.h"
+#include <emscripten.h>
+#include <google/vcdecoder.h>
+#include <string>
 
 int result[3];
 
 EMSCRIPTEN_KEEPALIVE
-int decode(uint8_t *dict, int dict_size, uint8_t *delta, int delta_size)
-{
-    std::string *target = new std::string();
-    std::string delta_str((char*)delta, delta_size);
-    open_vcdiff::VCDiffDecoder decoder;
-    decoder.Decode((char*)dict, dict_size, delta_str, target);
-    result[0] = (int)target->data();
-    result[1] = target->length();
-    result[2] = (int)delta;
-    return 0;
+int decode(uint8_t *dict, int dict_size, uint8_t *delta, int delta_size) {
+  std::string *target = new std::string();
+  std::string delta_str((char *)delta, delta_size);
+  open_vcdiff::VCDiffDecoder decoder;
+  decoder.Decode((char *)dict, dict_size, delta_str, target);
+  result[0] = (int)target->data();
+  result[1] = target->length();
+  result[2] = (int)target;
+  return 0;
 }
 
 EMSCRIPTEN_KEEPALIVE
-int free()
-{
-    if ((std::string *)result[2] != nullptr)
-        delete (std::string *)result[2];
-    return 0;
+int free() {
+  if ((std::string *)result[2] != nullptr)
+    delete (std::string *)result[2];
+  return 0;
 }
 
 EMSCRIPTEN_KEEPALIVE
-int get_result_pointer()
-{
-    return result[0];
-}
+int get_result_pointer() { return result[0]; }
 
 EMSCRIPTEN_KEEPALIVE
-int get_result_size()
-{
-    return result[1];
-}
+int get_result_size() { return result[1]; }
--- compat/vcdenc.cc
+++ compat/vcdenc.cc
@@ -1,13 +1,12 @@
-#include <emscripten.h>
-#include <string>
-#include <google/vcencoder.h>
 #include "common.h"
+#include <emscripten.h>
+#include <google/vcencoder.h>
+#include <string>
 
 int result[3];
 
 EMSCRIPTEN_KEEPALIVE
-int encode(uint8_t *dict, int dict_size, uint8_t *target, int target_size)
-{
+int encode(uint8_t *dict, int dict_size, uint8_t *target, int target_size) {
   open_vcdiff::VCDiffEncoder encoder((char *)dict, dict_size);
   encoder.SetFormatFlags(open_vcdiff::VCD_FORMAT_INTERLEAVED);
   std::string *delta = new std::string();
@@ -19,21 +18,14 @@ int encode(uint8_t *dict, int dict_size, uint8_t *target, int target_size)
 }
 
 EMSCRIPTEN_KEEPALIVE
-int free()
-{
+int free() {
   if ((std::string *)result[2] != nullptr)
     delete (std::string *)result[2];
   return 0;
 }
 
 EMSCRIPTEN_KEEPALIVE
-int get_result_pointer()
-{
-  return result[0];
-}
+int get_result_pointer() { return result[0]; }
 
 EMSCRIPTEN_KEEPALIVE
-int get_result_size()
-{
-  return result[1];
-}
+int get_result_size() { return result[1]; }
