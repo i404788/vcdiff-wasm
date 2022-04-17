fdff0c0ada6dd9795f2647d7ce7da84950dbe27c
i404788 <404788@student.fontys.nl>
2022-04-17 23:01:40 +0200

    v1.0.10

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 mkdir -p dist
 cd dist
 
 # Run emconfigure with the normal configure command as an argument.
-emconfigure cmake ../open-vcdiff  -Dvcdiff_build_test=OFF -Dvcdiff_build_exec=OFF -DCMAKE_BUILD_TYPE=MinSizeRel
+emcmake cmake ../open-vcdiff  -Dvcdiff_build_test=OFF -Dvcdiff_build_exec=OFF -DCMAKE_BUILD_TYPE=MinSizeRel
 
 # Run emmake with the normal make to generate linked LLVM bitcode.
 emmake make
@@ -13,8 +14,8 @@ emmake make
 #  [-Ox] represents build optimisations (discussed in the next section).
 export CPATH=$PWD/../open-vcdiff/src
 
-emcc -v -O3 -Os --llvm-lto 1 -s FILESYSTEM=0 -s SINGLE_FILE=1 -s ASSERTIONS=1 -s WASM=1 -s INLINING_LIMIT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s FILESYSTEM=0 ../compat/vcdenc.cc libvcdenc.a libvcdcom.a -o libvcdenc.js
-emcc -v -O3 -Os --llvm-lto 1  -s FILESYSTEM=0 -s SINGLE_FILE=1 -s ASSERTIONS=1 -s WASM=1 -s INLINING_LIMIT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s FILESYSTEM=0 ../compat/vcddec.cc libvcddec.a libvcdcom.a -o libvcddec.js
+emcc -v -Os -s FILESYSTEM=0 -s SINGLE_FILE=1 -s ASSERTIONS=1 -s WASM=1 -s INLINING_LIMIT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s FILESYSTEM=0 ../compat/vcdenc.cc libvcdenc.a libvcdcom.a -o libvcdenc.js
+emcc -v -Os -s FILESYSTEM=0 -s SINGLE_FILE=1 -s ASSERTIONS=1 -s WASM=1 -s INLINING_LIMIT=1 -s ALLOW_MEMORY_GROWTH=1 -s MODULARIZE=1 -s FILESYSTEM=0 ../compat/vcddec.cc libvcddec.a libvcdcom.a -o libvcddec.js
 
 # emcc -v -O3 -g0 -s WASM=0 -s MODULARIZE=1 ../compat/vcdenc.cc libvcdenc.a libvcdcom.a -o libvcdenc.js
 # emcc -v -O3 -g0 -s WASM=0 -s MODULARIZE=1 ../compat/vcddec.cc libvcddec.a libvcdcom.a -o libvcddec.js
